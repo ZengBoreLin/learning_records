c9d57012d94f2f1a473e65daf060f27359bf98c5
l-yx <3496708807@qq.com>
2024-11-01 22:40:56 +0800

    1

---
+++ lyx_a/rm_ws/build/pkg_b/CMakeFiles/listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/l/rm_ws/devel/.private/pkg_b/lib/pkg_b/listener"
+  "/home/l/rm_ws/devel/.private/pkg_b/lib/pkg_b/listener.pdb"
+  "CMakeFiles/listener.dir/listener.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
